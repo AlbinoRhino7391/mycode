1b715397feecf38f0df4cefb1cdefa4b82883a4a
AlbinoRhino7391 <ephelps52@gmail.com>
2023-07-11 19:29:04 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def main():
+
+    #create a list of animals
+    list1 = ["Fox", "Fly", "Ant", "Bee", "Cod"]
+
+    #display the list
+    print(list1)
+
+if __name__ == "__main__":
+    main()
