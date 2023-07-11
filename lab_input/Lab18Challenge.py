6d73fb93b9e659109ea1e2cbb0d5bb87953a6174
AlbinoRhino7391 <ephelps52@gmail.com>
2023-07-11 16:36:37 +0000

    join and split

---
+++ lab_input/Lab18Challenge.py
@@ -0,0 +1,8 @@
+def main():
+    name = input("What is your name?")
+    day_of_week = input("What day of the week it is?")
+
+    print("Hello, {}! Happy {}!".format(name, day_of_week))
+
+if __name__ == "__main__":
+    main()
