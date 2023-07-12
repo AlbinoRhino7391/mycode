015dafdba5af53c6f41e0cc33e52b409d37bf541
AlbinoRhino7391 <ephelps52@gmail.com>
2023-07-12 15:28:45 +0000

    working on if, elif, else logic

---
+++ iftest2/ipask.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   Conditionals - testing if strings test true"""
+
+ipchk = input("Apply an IP address: ") # this line now prompts the user for input
+
+# a provided string will test true
+if ipchk:
+   print("Looks like the IP address was set: " + ipchk) # indented under if
+else: #data is not provided
+    print("You did not provide an input") #meaningful whitespace is a pain
+
